df059a506b6faf5c4c86216ad24a0ef4aa8bb6ba
XsMagical <XsMagical@team-nocturnal.net>
2013-10-09 20:07:55 -0400

    Update BootCompleteReceiver.java

--- src/com/nocturnal/about/BootCompleteReceiver.java
+++ src/com/nocturnal/about/BootCompleteReceiver.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.crossbones.welcome;
+package com.nocturnal.about;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
